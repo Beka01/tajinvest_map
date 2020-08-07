70bb6fc6203428bc8bf30ceca4555f70cb5ca9ab
Beka01 <62505280+Beka01@users.noreply.github.com>
2020-08-07 22:21:40 +0500

    Add files via upload
    Map API

--- js/map.js
+++ js/map.js
@@ -43,4 +43,48 @@ $(document).ready(function () {
 
    getData();
 
+   const data = [
+      {
+         id: 7,
+         title: "Construction of a waste disposal plant in the Dushanbe city",
+         content: "The project will ensure the achievement of efficient waste treatment and improvement of the environmental situation in the city and suburbs of Dushanbe, obtaining compost, electricity and process steam (warm water) from alternative sources by burning waste.",
+         lat: 38.55440271784567,
+         lng: 68.96281965000003,
+      }, {
+         id: 8,
+         title: "Carrying out of drinking water line from the Dangara canal to the Free economic zone of Dangara",
+         content: "Carrying out of 14 km drinking water line from the Dangara canal to the Free economic zone of \u201cDangara\u201d and construction of two reservoirs with the volume of 4000 cubic meters.",
+         lat: 38.21571448056834,
+         lng: 69.23410293940424,
+      }, {
+         id: 9,
+         title: "Extraction of quartz sand in Dzhabor Rasulov district Sughd region",
+         content: "Quartz sand of the enterprise is used for the production of glass, glass products, cans, bottles, automotive glass and so on. Quartz sand is supplied in large volumes to the Republic of Uzbekistan and Kyrgyzstan.",
+         lat: 39.5155326,
+         lng: 69.09702300000004,
+      },
+   ]
+   function initMap() {
+      const mapProp = {
+         center: new google.maps.LatLng(38.554822, 68.776858),
+         zoom: 7,
+         mapTypeId: google.maps.MapTypeId.ROADMAP,
+      }
+      const map = new google.maps.Map(document.getElementById("googleMap"), mapProp)
+
+      const markers = []
+      data.forEach((el) => {
+         let latLng = new google.maps.LatLng(
+            el.lat,
+            el.lng
+         )
+         let marker = new google.maps.Marker({
+            position: latLng
+         })
+         markers.push(marker)
+      })
+      const markerCluster = new MarkerClusterer(map, markers)
+   }
+   initMap();
+
 });
