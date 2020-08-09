6fe5c988b4030e7df594918d04ac5e1a7dd428e9
Beka01 <62505280+Beka01@users.noreply.github.com>
2020-08-09 16:28:30 +0500

    Add files via upload
    Design Selectors

--- js/map.js
+++ js/map.js
@@ -1,6 +1,8 @@
-'use sctrict'
 
-$(function () {
+'use sctrict';
+$(document).ready(function () {
+
+   // Your web app's Firebase configuration
    var firebaseConfig = {
       apiKey: "AIzaSyBMLcSR6ZjtTIgI_-5b8QqDY_TgaBiJ9DQ",
       authDomain: "tajinvest-a77b7.firebaseapp.com",
@@ -10,66 +12,71 @@ $(function () {
       messagingSenderId: "257184881910",
       appId: "1:257184881910:web:ce9dccac5a8af6e25a20d7",
       measurementId: "G-7EETR5324V"
-   }
-   firebase.initializeApp(firebaseConfig)
+   };
+   // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
 
-   let lngCat
 
-   const database = firebase.database()
-   const rootRef = database.ref('map/sectors')
+   const title = document.getElementsByClassName('card-title');
+   const description = document.getElementsByClassName('card-text');
+   const category = document.getElementsByClassName('card-text');
+   let lngCat;
+
+   const database = firebase.database();
+   const rootRef = database.ref('map/sectors');
 
    function getData() {
       let sectorName = null
       $(".data").empty()
-      rootRef.once("value").then(function(snapshot) {
-         snapshot.forEach(function(childSnapshot) {
-
+      $(".category").empty()
+      rootRef.once("value").then(function (snapshot) {
+         snapshot.forEach(function (childSnapshot, catId) {
             let sector = childSnapshot.val()
             sectorName = sector.name[lngCat]
-
-            snapshot.forEach(function(childSnapshot, catId){
-               elfilcat = `<option value="${catId}">${sectorName}</option>`
-               $(".form-control category").append(elfilcat)
-            })
+            elfilcat = `
+               <option value="${catId}">${sectorName}</option>`
+            $(".category").append(elfilcat)
 
             sector.projects.forEach(el => {
                element = `
-                  <div class="card">
-                     <div class="card-body">
-                        <h5 class="card-title">${el.title[lngCat]}</h5>
-                        <p class="card-text">${el.descr[lngCat]}</p>
-                        <span class = "badge badge-primary">${sectorName}</span> 
-                        <span class = "badge badge-primary">${el.cost}</span> 
-                     </div>
-                  </div>`
+                      <div class="card">
+                         <div class="card-body">
+                            <h5 class="card-title">${el.title[lngCat]}</h5>
+                            <p class="card-text">${el.descr[lngCat]}</p>
+                            <span class = "badge badge-primary">${sectorName}</span> 
+                            <span class = "badge badge-primary">${"$ " + el.cost + " mln"} </span> 
+                         </div>
+                      </div>`
                $(".data").append(element)
+
             })
          })
-      })      
+      })
    }
 
-   getCategories()
-   
-   function getCategories (){
+   getCategories();
+   function getCategories() {
       lngCat = $("#select option:selected").val()
       console.log(lngCat)
    }
-   
+
    $("#select").on("change", () => {
-      $('.data').empty()
+      $('.data').empty();
       getData()
       getCategories()
    })
 
-   getData()
-   
+   getData();
 
+  
    //MAP 
    function initMap() {
       const mapProp = {
          center: new google.maps.LatLng(38.554822, 68.776858),
          zoom: 7,
          mapTypeId: google.maps.MapTypeId.ROADMAP,
+         mapTypeControl: false,
+         streetViewControl:false,
       }
       const map = new google.maps.Map(document.getElementById("googleMap"), mapProp)
 
@@ -86,5 +93,6 @@ $(function () {
       })
       const markerCluster = new MarkerClusterer(map, markers)
    }
-   initMap()
-})
+   initMap();
+
+});
