1dca22b5787fc0eb8f1e962cd84865111155f652
Vitek Foxprogs <foxprogs@gmail.com>
2020-08-10 00:52:42 +0600

    update code

--- js/map.js
+++ js/map.js
@@ -1,5 +1,11 @@
 'use sctrict'
 
+let map
+let markerClusterer = null
+let markers = []
+let sectors = []
+let projects = []
+
 $(function () {
 
   let firebaseConfig = {
@@ -15,57 +21,120 @@ $(function () {
   firebase.initializeApp(firebaseConfig)
 
   let currentLang
-  let sectors = []
-  let projects = []
 
   const database = firebase.database()
   const rootRef = database.ref('map/sectors')
 
+  /* здесь мы с firebase получаем все данные и создаем два массива. Массив с категориями и массив с проектами*/
   function getData() {
-    let sectorName = null
-    $(".data").empty()
-    $(".category").empty()
-    rootRef.once("value").then(function (sectorsData) {
-      sectorsData.forEach(function (sector, sectorId) {
-        sectors.push(sector.val().name)
-
-        sectorName = sector.val().name[currentLang]
-
-        elfilcat = `<option value="${sectorId}">${sectorName}</option>`
-        $(".form-control category").append(elfilcat)
-
-        sector.val().projects.forEach((projectData, projectId) => {
-          projectData.sector = sectorId
-          projects.push(projectData)
-
-          projectCard = `
-            <div id="${projectId}" class="card project">
-              <div class="card-body">
-                <h5 class="card-title">${projectData.title[currentLang]}</h5>
-                <p class="card-text">${projectData.descr[currentLang]}</p>
-                <span class = "badge badge-primary">${sectorName}</span> 
-                <span class = "badge badge-primary">${projectData.cost}</span> 
-              </div>
-            </div>`
-          $(".data").append(projectCard)
+    rootRef.once("value").then(function (sectorsData) { // получили все данные с базы
+      sectorsData.forEach(function (sector) { // проходим по каждому сектору
+        sectors.push(sector.val().name) // запишем данные в массив для последующих манипуляций
+        sector.val().projects.forEach((projectData, projectId) => { // пройдем по всем проектам сектора
+          projectData.sectorId = sector.key
+          projects.push(projectData) // для связи проекта с сектором укажем его айди
         })
       })
+    }).then(() => {
+      createPage()      
     })
   }
 
-  getLang()
+  /* первоначальное создание страницы */
+  function createPage(){
+    setSectors()
+    setProjects()
+  }
+
+  /* проходим массив с проектами и создаем card а так же наносим метки на карту */
+  function setProjects(){
+    projects.forEach((project, projectId) => {
+      projectCard = `
+        <div id="project${projectId}" class="card project">
+          <div class="card-body">
+            <h5 class="card-title title">${project.title[currentLang]}</h5>
+            <p class="card-text descr">${project.descr[currentLang]}</p>
+            <span class="badge badge-primary project-sector">${sectors[project.sectorId][currentLang]}</span>
+            <span class="badge badge-primary">${project.cost}</span> 
+          </div>
+        </div>`
+      $(".projects").append(projectCard) // добавим карту на страницу
+      addMarkerToClusterer(project)
+    })
+
+    setMarkerClusterer()
+  }
+
+  /* первоначальная установка ввыпадающий список секторов */
+  function setSectors(){
+    sectors.forEach((sector, sectorId) => {
+      sectorOption = `<option id="sector${sectorId}" value="${sectorId}">${sector[currentLang]}</option>`
+      $(".sectors").append(sectorOption)
+    })
+  }
   
   function getLang(){
     currentLang = $("#select option:selected").val()
   }
 
+  getLang()
+  getData()
+
   $("#select").on("change", () => {
-    $('.data').empty()
-    getData()
+    /* при изменении языка сначала мы его устанавливаем, потом обновляем первод у секторов и у проектов */
     getLang()
+    updateSectors()
+    updateProjects()
   })
 
-  getData()
+  /* по айдишнику мы одновляем текст у секторов */
+  function updateSectors(){
+    sectors.forEach((sector, sectorId) => {
+      $("#sector"+sectorId).text(sector[currentLang])
+    })
+  }
+
+  /* по айдишнику обновляем текст у проектов. Учитывать фильтры надо */
+  function updateProjects(){
+    if (markerClusterer) { /* если на карте есть какие-то отметки, удаляем их */
+      markerClusterer.clearMarkers()
+      markers = []
+    }
+
+    let currentProject
+    projects.forEach((project, projectId) => {
+      currentProject = $("#project"+projectId)
+      currentProject.find('.title').text(project.title[currentLang])
+      currentProject.find('.descr').text(project.descr[currentLang])
+      currentProject.find('.title').text(sectors[project.sectorId][currentLang])
+      addMarkerToClusterer(project)
+    })
+
+    setMarkerClusterer()
+  }
+
+  function setMarkerClusterer()
+  {
+    markerClusterer = new MarkerClusterer(map, markers, {imagePath: './images/m'})
+  }
+
+  function addMarkerToClusterer(project){
+    let latLng = new google.maps.LatLng(
+      project.lat,
+      project.lon,
+    )
+
+    let infowindow = new google.maps.InfoWindow({
+      content: `<h1>${project.title[currentLang]}</h1><p>${project.descr[currentLang]}</p>`,
+    })
+
+    let marker = new google.maps.Marker({
+      position: latLng,
+      icon:'https://map.investcom.tj/wp-content/plugins/wp-google-map-gold/assets/images/icons/factory.png',
+    })
+    marker.addListener("click", () => infowindow.open(map, marker))
+    markers.push(marker)
+  }
 
   function initMap() {
     const mapProp = {
@@ -75,20 +144,8 @@ $(function () {
       mapTypeControl: false,
       streetViewControl:false,
     }
-    const map = new google.maps.Map(document.getElementById("googleMap"), mapProp)
-
-    const markers = []
-    projects.forEach((el) => {
-      let latLng = new google.maps.LatLng(
-        el.lat,
-        el.lng
-      )
-      let marker = new google.maps.Marker({
-        position: latLng
-      })
-      markers.push(marker)
-    })
-    const markerCluster = new MarkerClusterer(map, markers, {imagePath: './images/m'})
+    map = new google.maps.Map(document.getElementById("googleMap"), mapProp)
   }
+
   initMap()
 })
